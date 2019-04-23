98626aed3519eb306a6eda72b93c9a20ea1f0424
Anthony Lopez Vega <anthony.lopez@administradors-iMac.local>
2019-04-23 08:39:26 -0500

    fix route

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -6,31 +6,31 @@ import { Task } from './interfaces/task.interface';
 import { async } from 'rxjs/internal/scheduler/async';
 import { throws } from 'assert';
 
-@Controller('tasks')
+@Controller('api')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
+  @Post('/tasks')
   async create(@Body() createTaskDto: CreateTaskDto) {
     this.tasksService.create(createTaskDto);
   }
 
-  @Get()
+  @Get('/tasks')
   async findTasks(): Promise<Task[]> {
     return this.tasksService.findTasks();
   }
 
-  @Get('/:id')
+  @Get('/tasks/:id')
   async findTask(@Param('id') id: string) {
     return await this.tasksService.findTask(id);
   }
 
-  @Put('/:id')
+  @Put('/tasks/:id')
   async updateTask(@Param('id') id: string, @Body() task: Partial<Task>) {
     return await this.tasksService.updateTask(id, task);
   }
 
-  @Delete('/:id')
+  @Delete('/tasks/:id')
   async deleteTask(@Param('id') id: string) {
     return await this.tasksService.deleteTask(id);
   }
